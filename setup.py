d17c56d275c8027ad58274872539b879a3841f1e
Tomer Klein <tomer.klein@gmail.com>
2023-01-18 18:27:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,47 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
+
+with open("README.md", "r", encoding="UTF-8") as f:
+     readme = f.read()
 
 setup_args = dict(
     name='weatheril',
-    version='1.0.0',
+    version='0.5.0',
     description='Israel Meteorological Service unofficial python api wrapper',
     long_description_content_type="text/markdown",
+    long_description=readme,
     license='MIT',
     packages=find_packages(),
     author='Tomer Klein',
     author_email='tomer.klein@gmail.com',
     keywords=['ims', 'weatheril', 'Israel Meteorological Service','Meteorological Service','weather'],
     url='https://github.com/t0mer/py-weatheril',
-    download_url='https://pypi.org/project/weatheril/'
+    download_url='https://pypi.org/project/weatheril/',
+    project_urls={
+        "Documentation": "https://github.com/t0mer/py-weatheril",
+        "Source": "https://github.com/t0mer/py-weatheril",
+    },
+    python_requires=">=3.6",
+    install_requires=["requests",
+                    "pillow",
+                    "pandas",
+                    "urllib3"],
+    classifiers=[
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+],
 )
 
-install_requires = [
-    'Pillow',
-    'pandas',
-    'requests',
-    'urllib3'
-    
-]
+
 
 if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
+    setup(**setup_args)
