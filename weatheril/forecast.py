4051c359a05ba7c6f17a28a8ac9e0b62fa6be14a
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2023-02-13 17:18:20 +0200

    fix duplicate data in Forecast (#13)

--- weatheril/forecast.py
+++ weatheril/forecast.py
@@ -4,24 +4,11 @@ from datetime import datetime
 
 
 class Forecast:
-    days: list
-
     def __init__(self,days: list = []):
         self.days = days
 
 
 class Daily:
-    date: datetime
-    location: str
-    day: str
-    weather: str
-    weather_code: str
-    minimum_temperature: int
-    maximum_temperature: int
-    maximum_uvi: int
-    hours: list
-    description: str
-
     def __init__(self, date: datetime, location: str, day: str,weather: str,weather_code: str, minimum_temperature: int, maximum_temperature: int, maximum_uvi: int, description: str, hours: list = []) -> None:
         self.date = date
         self.location = location
@@ -36,12 +23,6 @@ class Daily:
 
 
 class Hourly:
-    hour: str
-    forecast_time: datetime
-    weather: str
-    weather_code: str
-    temperature: int
-
     def __init__(self,hour: str,forecast_time: datetime,weather: str,weather_code: str, temperature: int) -> None:
         self.hour = hour
         self.forecast_time = forecast_time
