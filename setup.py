1488fbb5d1b77f9f7d9c2a0efebff73159a5ddfc
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2023-05-14 09:52:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="UTF-8") as f:
 
 setup_args = dict(
     name='weatheril',
-    version='0.27.0',
+    version='0.28.0',
     description='Israel Meteorological Service unofficial python api wrapper',
     long_description_content_type="text/markdown",
     long_description=readme,
