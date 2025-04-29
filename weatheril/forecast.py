406d47a787acf5e4d5d9bda31adc57a81c944194
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2025-04-29 09:38:57 +0300

    feat: Update models with recent API changes (#31)
    * feat: Update models with recent API changes
    
    * style: Ruff styling

--- weatheril/forecast.py
+++ weatheril/forecast.py
@@ -3,23 +3,31 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from datetime import datetime
 from json import JSONEncoder
+from typing import Optional
 
-from .utils import get_location_name_by_id, get_day_of_the_week, get_weather_description_by_code, get_wind_direction
+from .utils import (
+    get_location_name_by_id,
+    get_day_of_the_week,
+    get_weather_description_by_code,
+    get_wind_direction,
+)
 
 
 @dataclass
 class Forecast:
     days: list[Daily] = field(default_factory=list)
 
+
 @dataclass
 class Daily:
     language: str
     date: datetime
     lid: str
-    weather_code: str
+    weather_code: Optional[int]
     minimum_temperature: int
     maximum_temperature: int
     maximum_uvi: int
+    u_v_i_factor: float
     description: str
     hours: list[Hourly] = field(default_factory=list)
     day: str = field(init=False)
@@ -31,28 +39,30 @@ class Daily:
         self.location = get_location_name_by_id(self.language, self.lid)
         self.weather = get_weather_description_by_code(self.language, self.weather_code)
 
+
 @dataclass
 class Hourly:
     language: str
     hour: str
     forecast_time: datetime
-    temperature: int
-    precise_temperature: float
-    weather_code: str
-    heat_stress: float
-    heat_stress_level: int
-    relative_humidity: int
-    rain: float
-    rain_chance: float
-    wind_speed: int
-    wind_direction_id: int
-    wind_chill: int
+    created: datetime
+    temperature: Optional[int]
+    precise_temperature: Optional[float]
+    weather_code: Optional[int]
+    heat_stress: Optional[float]
+    heat_stress_level: Optional[int]
+    pm10: Optional[int]
+    relative_humidity: Optional[int]
+    rain: Optional[float]
+    rain_chance: Optional[float]
+    wind_speed: Optional[int]
+    wind_direction_id: Optional[int]
+    wind_chill: Optional[int]
     weather: str = field(init=False)
     wind_direction: str = field(init=False)
-    wave_height: float
+    wave_height: Optional[float]
     u_v_index: int
-    u_v_i_max: int
-
+    u_v_i_max: Optional[int]
 
     def __post_init__(self):
         self.weather = get_weather_description_by_code(self.language, self.weather_code)
--- weatheril/radar_satellite.py
+++ weatheril/radar_satellite.py
@@ -9,6 +9,7 @@ from loguru import logger
 from urllib.parse import urlparse
 from dataclasses import dataclass, field
 
+
 @dataclass
 class RadarSatellite:
     imsradar_images: list
@@ -16,45 +17,73 @@ class RadarSatellite:
     middle_east_satellite_images: list
     europe_satellite_images: list
 
-    def __init__(self,imsradar_images: list=[], radar_images: list=[],middle_east_satellite_images: list=[],europe_satellite_images: list=[]):
+    def __init__(
+        self,
+        imsradar_images: list = [],
+        radar_images: list = [],
+        middle_east_satellite_images: list = [],
+        europe_satellite_images: list = [],
+    ):
         self.imsradar_images = imsradar_images
         self.radar_images = radar_images
         self.middle_east_satellite_images = middle_east_satellite_images
         self.europe_satellite_images = europe_satellite_images
 
-    def generate_images(self,path:str = ""):
-        self.create_animation("imsradar.gif",self.imsradar_images,path)
-        self.create_animation("radar.gif",self.radar_images,path)
-        self.create_animation("middle_east.gif",self.middle_east_satellite_images,path)
-        self.create_animation("europe.gif",self.europe_satellite_images,path)
-        
-    def create_animation(self, animated_file:str, images: list,path:str ):
-        '''
+    def generate_images(self, path: str = ""):
+        self.create_animation("imsradar.gif", self.imsradar_images, path)
+        self.create_animation("radar.gif", self.radar_images, path)
+        self.create_animation(
+            "middle_east.gif", self.middle_east_satellite_images, path
+        )
+        self.create_animation("europe.gif", self.europe_satellite_images, path)
+
+    def create_animation(self, animated_file: str, images: list, path: str):
+        """
         This method will download the images needed to create animated Radar / Satellite image
         parameters:
             >>> path: path to save the animated image. if path wil not be provided, the default path will be the current one.
-            >>> animated_file: The name of the animated file 
+            >>> animated_file: The name of the animated file
             >>> images: the list of images for creating the animation.
-        '''
+        """
         try:
             if os.path.exists(path):
-               animated_image_path = path + "/" + animated_file
+                animated_image_path = path + "/" + animated_file
             else:
-                animated_image_path = os.path.realpath(os.path.dirname(__file__)) + "/" + animated_file
-            logger.debug('Creating ' + animated_file + ' animation at: ' + animated_image_path)
+                animated_image_path = (
+                    os.path.realpath(os.path.dirname(__file__)) + "/" + animated_file
+                )
+            logger.debug(
+                "Creating " + animated_file + " animation at: " + animated_image_path
+            )
 
             for idx, item in enumerate(images):
                 file = requests.get(images[idx])
-                open(tempfile.gettempdir() + "/" + os.path.basename(urlparse(images[idx]).path), "wb").write(file.content)
-                images[idx] = tempfile.gettempdir() + "/" +  os.path.basename(urlparse(images[idx]).path)
-            
+                open(
+                    tempfile.gettempdir()
+                    + "/"
+                    + os.path.basename(urlparse(images[idx]).path),
+                    "wb",
+                ).write(file.content)
+                images[idx] = (
+                    tempfile.gettempdir()
+                    + "/"
+                    + os.path.basename(urlparse(images[idx]).path)
+                )
+
             frames = [Image.open(image) for image in images]
             frame_one = frames[0]
-            frame_one.save(animated_image_path, format="GIF", append_images=frames,save_all=True, duration=4, loop=0)
+            frame_one.save(
+                animated_image_path,
+                format="GIF",
+                append_images=frames,
+                save_all=True,
+                duration=4,
+                loop=0,
+            )
 
             for image in images:
                 os.remove(image)
             return animated_image_path
         except Exception as e:
-            logger.error('Error creating ' + animated_file + ' animation. ' + str(e))
+            logger.error("Error creating " + animated_file + " animation. " + str(e))
             return None
